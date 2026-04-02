8b8039dd2d4af3c1581f6089496ad845c54fd884
saida <172148277+saidasaidali@users.noreply.github.com>
2026-04-02 14:10:18 +0100

    fix db config with neon

--- cleannow-backend/config/db.js
+++ cleannow-backend/config/db.js
@@ -1,18 +1,17 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-
-console.log("DEBUG DB PASSWORD:", process.env.DB_PASSWORD, typeof process.env.DB_PASSWORD);
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  String(process.env.DB_PASSWORD), // <-- ajoute .toString()
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT), // utilise la variable d'env
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // nécessaire pour Neon
+    },
+  },
+});
 
 const connectDB = async () => {
   try {
